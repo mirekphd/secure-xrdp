5c68c5e3960a087dead7e0c1ca1b04bd8a5530f3
mirekphd <36706320+mirekphd@users.noreply.github.com>
2018-08-04 11:39:19 +0200

    grant user ownership to xrdp and xrdp-sesman logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ dbus-daemon --system
 # create the sock dir properly
 /bin/sh /usr/share/xrdp/socksetup
 
+# grant user ownership to the xrdp and xrdp-sesman log files
+chown 1000 /var/log/xrdp.log
+chown 1000 /var/log/xrdp-sesman.log
+
 # run xrdp and xrdp-sesman in the foreground so the logs show in docker
 xrdp-sesman -ns &
 xrdp -ns
