97a020358fe80cb6b43a521a72feae66c8a57516
mirekphd <36706320+mirekphd@users.noreply.github.com>
2018-08-05 15:45:21 +0200

    removed dbus-launch (cannot be run before loggin in and creating an x11 session)

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,12 +16,16 @@ xrdp -ns
 # caution: we cannot create a system-wide bus (with --system), 
 # because that would require root, so instead we create a per-login-session 
 # message bus (with --session), which can be run as standard user
+# dbus-daemon --system
+# dbus-daemon --session
+
 # caution: we cannot use dbus-daemon -session, as it does not exit, 
 # and would prevent xrdp from being run;
 # instead we use dbus-launch: a utility to start D-Bus message bus daemon from a shell script,
 # and unlike the daemon itself, dbus-launch exits, allowing subsequent programs (such as xrdp) to run;
 # note also that we must specify which session should be used (e.g. xfce4-session)
-# dbus-daemon --system
-# dbus-daemon --session
+# caution: dbus-launch cannot be executed until an X11 session has been created,
+# so running it here would cause errors like this:
+# "xfce4-session: Cannot open display: ."
 # dbus-launch --exit-with-session
-dbus-launch --exit-with-session xfce4-session
+# dbus-launch --exit-with-session xfce4-session
